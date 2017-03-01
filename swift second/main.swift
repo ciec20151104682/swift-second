31de52bdd2b53e2d4a4a7d9be055f4e83c9f92db
ciec20151104682 <s20151104682@mac21.local>
2017-03-01 10:04:56 +0800

    Initial Commit

---
+++ swift second/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift second
+//
+//  Created by s20151104682 on 17/3/1.
+//  Copyright © 2017年 s20151104682. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
